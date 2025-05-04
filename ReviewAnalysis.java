d4ce584490ea78d4642227ee2d5b2789a99a35d2
Lance Phoenix <108348802+lancepr1@users.noreply.github.com>
2025-05-04 17:43:10 -0400

    Create ReviewAnalysis.java

---
+++ ReviewAnalysis.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class ReviewAnalysis
+{
+    ArrayList<String> comments = new ArrayList<String>();
+    private Review[] allReviews;
+    public ReviewAnalysis(Review[] reviews)
+    {
+        allReviews = reviews;  
+    }
+    public double getAverageRating()
+    {
+        double sum = 0;
+        for(Review r : allReviews)
+        {
+            sum += r.getRating();
+        }
+        return sum / allReviews.length;
+    }
+    
+    public ArrayList<String> collectComments()
+    {
+        for (int i = 0; i < allReviews.length; i++)
+        {
+            String s = i + "-" + allReviews[i].getComment();
+            String last = s.substring(s.length()-1);
+            if(!last.equals(".") && !last.equals("!"))
+            {
+                s += ".";
+            }
+            if(s.indexOf("!")> 0) comments.add(s);
+        }
+        return comments;
+    }
+}
